e863e4005bebcc18b10752a7a6f7605757c0b8fb
infinitygp <infinitywar9@gmail.com>
2026-08-09 13:33:13 +0200

    chore: prepare v0.3.1 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
 		applicationId = "dev.dunehd.jellyfinbridge"
 		minSdk = 21
 		targetSdk = 36
-		versionCode = 3
-		versionName = "0.3.0"
+		versionCode = 4
+		versionName = "0.3.1"
 	}
 
 	signingConfigs {
